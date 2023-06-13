scylladb/scylla-cqlsh

commit ad1c8d8e99a03614c8bf136c568fb59b2112bc3f

Support using other driver (datastax or older scylla ones)
Make sure we don't try to use the driver with arguments it
doesn't support, and alert the use if a wrong version is
used to try to connect with scylla cloud bundle.

Fix: #19

bin/cqlsh.py
@@ -141,6 +141,7 @@ from cassandra.metadata import (ColumnMetadata, KeyspaceMetadata, TableMetadata,
 from cassandra.policies import WhiteListRoundRobinPolicy
 from cassandra.query import SimpleStatement, ordered_dict_factory, TraceUnavailable
 from cassandra.util import datetime_from_timestamp
+from cassandra.connection import DRIVER_NAME, DRIVER_VERSION
 
 # cqlsh should run correctly when run out of a Cassandra source tree,
 # out of an unpacked Cassandra tarball, and after a proper package install.
@@ -484,9 +485,11 @@ class Shell(cmd.Cmd):
             }
 
             if cloudconf is None:
+                assert 'scylla' in DRIVER_NAME.lower(), f"{DRIVER_NAME} {DRIVER_VERSION} isn't supported by scylla_cloud"
                 kwargs['contact_points'] = (self.hostname,)
                 kwargs['port'] = self.port
                 kwargs['ssl_options'] = sslhandling.ssl_settings(hostname, CONFIG_FILE) if ssl else None
+                kwargs['scylla_cloud'] = cloudconf
                 profiles[EXEC_PROFILE_DEFAULT].load_balancing_policy = WhiteListRoundRobinPolicy([self.hostname])
 
             self.conn = Cluster(cql_version=cqlver,
@@ -494,7 +497,6 @@ class Shell(cmd.Cmd):
                                 control_connection_timeout=connect_timeout,
                                 connect_timeout=connect_timeout,
                                 execution_profiles=profiles,
-                                scylla_cloud=cloudconf,
                                 **kwargs)
         self.owns_connection = not use_conn
 
@@ -2124,12 +2126,13 @@ class Shell(cmd.Cmd):
                 kwargs['port'] = self.port
                 kwargs['ssl_options'] = self.conn.ssl_options
                 kwargs['load_balancing_policy'] = WhiteListRoundRobinPolicy([self.hostname])
+                kwargs['scylla_cloud'] = self.cloudconf
+
         conn = Cluster(cql_version=self.conn.cql_version,
                        protocol_version=self.conn.protocol_version,
                        auth_provider=auth_provider,
                        control_connection_timeout=self.conn.connect_timeout,
                        connect_timeout=self.conn.connect_timeout,
-                       scylla_cloud=self.cloudconf,
                        **kwargs)
 
         if self.current_keyspace:

commit 254bf3f80534821d428be816d01c1ef7ada509ef

Fix cloudconf handling
the change in ad1c8d8e99a03614c8bf136c568fb59b2112bc3f
broke the cloudconf option, since it mixed it with all
the other parameter which are not working together
with the cloud bundle options, and it need to be on
it's own branch of the code.

bin/cqlsh.py
@@ -485,12 +485,13 @@ class Shell(cmd.Cmd):
             }
 
             if cloudconf is None:
-                assert 'scylla' in DRIVER_NAME.lower(), f"{DRIVER_NAME} {DRIVER_VERSION} isn't supported by scylla_cloud"
                 kwargs['contact_points'] = (self.hostname,)
                 kwargs['port'] = self.port
                 kwargs['ssl_options'] = sslhandling.ssl_settings(hostname, CONFIG_FILE) if ssl else None
-                kwargs['scylla_cloud'] = cloudconf
                 profiles[EXEC_PROFILE_DEFAULT].load_balancing_policy = WhiteListRoundRobinPolicy([self.hostname])
+            else:
+                assert 'scylla' in DRIVER_NAME.lower(), f"{DRIVER_NAME} {DRIVER_VERSION} isn't supported by scylla_cloud"
+                kwargs['scylla_cloud'] = cloudconf
 
             self.conn = Cluster(cql_version=cqlver,
                                 auth_provider=self.auth_provider,
@@ -2122,11 +2123,12 @@ class Shell(cmd.Cmd):
 
         kwargs = {}
         if self.cloudconf is None:
-                kwargs['contact_points'] = (self.hostname,)
-                kwargs['port'] = self.port
-                kwargs['ssl_options'] = self.conn.ssl_options
-                kwargs['load_balancing_policy'] = WhiteListRoundRobinPolicy([self.hostname])
-                kwargs['scylla_cloud'] = self.cloudconf
+            kwargs['contact_points'] = (self.hostname,)
+            kwargs['port'] = self.port
+            kwargs['ssl_options'] = self.conn.ssl_options
+            kwargs['load_balancing_policy'] = WhiteListRoundRobinPolicy([self.hostname])
+        else:
+            kwargs['scylla_cloud'] = self.cloudconf
 
         conn = Cluster(cql_version=self.conn.cql_version,
                        protocol_version=self.conn.protocol_version,